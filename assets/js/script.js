3c99b4b6fbe9fd20518c27760f8d02202f594f2f
Toan <nguyentoan0275@gmail.com>
2023-10-30 15:50:46 -0400

    added async function for getting location and saving it as a string

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 // https://api.openweathermap.org/geo/1.0/direct?q=New+Brunswick&limit=1&appid=161b355a9dd41b55204725cd903f95ab
 // [{"name":"New Brunswick","local_names":{"en":"New Brunswick"},"lat":40.4862174,"lon":-74.4518173,"country":"US","state":"New Jersey"}]
 
-// weather api call to get weather of a longitude and latitude 
+// weather api call to get weather of a longitude and latitude
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={API key}
 // example is New Brunswick's weather
 // https://api.openweathermap.org/data/2.5/weather?lat=40.4862174&lon=-74.4518173&units=imperial&appid=161b355a9dd41b55204725cd903f95ab
@@ -14,61 +14,104 @@
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid={API key}
 // example is New Brunswick 5 day forecast
 // https://api.openweathermap.org/data/2.5/forecast?lat=40.4862174&lon=-74.4518173&units=imperial&appid=161b355a9dd41b55204725cd903f95ab
-// 
-let searchButton = document.getElementById('search')
-searchButton.addEventListener('click', weatherToday)
-function weatherToday(){
-    //create
-    todayContainer = document.createElement('div')
-    todayDate = document.createElement('h1')
-    weatherContainer = document.createElement('ul')
-    temp = document.createElement('li')
-    wind = document.createElement('li')
-    humidity = document.createElement('li')
-    //attr
-    todayContainer.classList.add('today')
-    todayDate.textContent = "Today's data"
-    weatherContainer.classList.add('today')
-    temp.textContent = 'Temperature'
-    wind.textContent = 'Wind'
-    humidity.textContent = 'Humidity'
-    //append
-    document.body.appendChild(todayContainer)
-    todayContainer.appendChild(todayDate)
-    todayContainer.appendChild(weatherContainer)
-    weatherContainer.appendChild(temp)
-    weatherContainer.appendChild(wind)
-    weatherContainer.appendChild(humidity)
+//
 
-    weatherForecast()
-}
+$(function () {
+  // 'https://api.openweathermap.org/geo/1.0/direct?q=' + '{user-input}' + '&limit=1&appid=161b355a9dd41b55204725cd903f95ab'
+  let locationURL =
+    "https://api.openweathermap.org/geo/1.0/direct?q=New+Brunswick&limit=1&appid=161b355a9dd41b55204725cd903f95ab";
 
-function weatherForecast(){
-    forecastContainer = document.createElement('div')
-    forecastText = document.createElement('h2')
-    for (let i = 0; i < 5; i++) {
-        //create
-        forecastBox = document.createElement('div')
-        forecastDate = document.createElement('h3')
-        forecastIcon = document.createElement('div')
-        forecastTemp = document.createElement('li')
-        forecastWind = document.createElement('li')
-        forecastHumidity = document.createElement('li')
+  async function requestLocation() {
+    let response = await fetch(locationURL);
+    let jsonData = await response.json();
+    // console.log(jsonData[0].lat)
+    // console.log(jsonData[0].lon)
+    // console.log(Object.keys(jsonData))
+    // console.log(typeof(jsonData))
+    var latitude = "lat=" + String(jsonData[0].lat) + "&";
+    var longitude = "lon=" + String(jsonData[0].lon);
+    requestWeather(latitude, longitude)
+  }
+  // pass longitude and latitude down here
+  // let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?' + latitude + longitude +  '&units=imperial&appid=161b355a9dd41b55204725cd903f95ab';
+  // let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=40.4862174&lon=-74.4518173&units=imperial&appid=161b355a9dd41b55204725cd903f95ab'
+  async function requestWeather(param1, param2) {
+    await requestLocation()
+    let weatherURL =
+      "https://api.openweathermap.org/data/2.5/weather?" +
+      param1 +
+      param2 +
+      "&units=imperial&appid=161b355a9dd41b55204725cd903f95ab";
+    let response = await fetch(weatherURL);
+    let jsonData = await response.json();
+    console.log(Object.keys(jsonData));
+  }
+  requestWeather()
 
-        //attr
-        forecastBox.classList.add('forecast')
-        forecastDate.textContent = 'Forecast Date'
-        forecastIcon.textContent = 'cloudy'
-        forecastTemp.textContent = 'Forecast Temp'
-        forecastWind.textContent = 'Forecast Wind'
-        forecastHumidity.textContent = 'Forecast Humidity'
+  // $(function() {
+  //     let forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=40.4862174&lon=-74.4518173&units=imperial&appid=161b355a9dd41b55204725cd903f95ab';
 
-        //append
-        document.body.appendChild(forecastBox)
-        forecastBox.appendChild(forecastDate)
-        forecastBox.appendChild(forecastIcon)
-        forecastBox.appendChild(forecastTemp)
-        forecastBox.appendChild(forecastWind)
-        forecastBox.appendChild(forecastHumidity)
-    }
-}
+  //     async function requestForecast() {
+  //         let response = await fetch(forecastURL);
+  //         let jsonData = await response.json();
+  //         console.log(Object.keys(jsonData))
+  //     }
+});
+
+// let searchButton = document.getElementById('search')
+// searchButton.addEventListener('click', weatherToday)
+// function weatherToday(){
+//     //create
+//     todayContainer = document.createElement('div')
+//     todayDate = document.createElement('h1')
+//     weatherContainer = document.createElement('ul')
+//     temp = document.createElement('li')
+//     wind = document.createElement('li')
+//     humidity = document.createElement('li')
+//     //attr
+//     todayContainer.classList.add('today')
+//     todayDate.textContent = "Today's data"
+//     weatherContainer.classList.add('today')
+//     temp.textContent = 'Temperature'
+//     wind.textContent = 'Wind'
+//     humidity.textContent = 'Humidity'
+//     //append
+//     document.body.appendChild(todayContainer)
+//     todayContainer.appendChild(todayDate)
+//     todayContainer.appendChild(weatherContainer)
+//     weatherContainer.appendChild(temp)
+//     weatherContainer.appendChild(wind)
+//     weatherContainer.appendChild(humidity)
+
+//     weatherForecast()
+// }
+
+// function weatherForecast(){
+//     forecastContainer = document.createElement('div')
+//     forecastText = document.createElement('h2')
+//     for (let i = 0; i < 5; i++) {
+//         //create
+//         forecastBox = document.createElement('div')
+//         forecastDate = document.createElement('h3')
+//         forecastIcon = document.createElement('div')
+//         forecastTemp = document.createElement('li')
+//         forecastWind = document.createElement('li')
+//         forecastHumidity = document.createElement('li')
+
+//         //attr
+//         forecastBox.classList.add('forecast')
+//         forecastDate.textContent = 'Forecast Date'
+//         forecastIcon.textContent = 'cloudy'
+//         forecastTemp.textContent = 'Forecast Temp'
+//         forecastWind.textContent = 'Forecast Wind'
+//         forecastHumidity.textContent = 'Forecast Humidity'
+
+//         //append
+//         document.body.appendChild(forecastBox)
+//         forecastBox.appendChild(forecastDate)
+//         forecastBox.appendChild(forecastIcon)
+//         forecastBox.appendChild(forecastTemp)
+//         forecastBox.appendChild(forecastWind)
+//         forecastBox.appendChild(forecastHumidity)
+//     }
+// }
